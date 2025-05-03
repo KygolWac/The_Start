b96060f9210e815da52a8e8091cefba1595062a8
zzh <1030509669@qq.com>
2025-05-04 04:40:28 +0800

    new moving plantforms

---
+++ Assets/Scripts/PlantformMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantformMove : MonoBehaviour
+{
+    public Transform posA, posB;
+    private Transform movePos;
+    [SerializeField] private float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePos = posA;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, posA.position) < 0.1f)
+        {
+            movePos = posB;
+        }
+        if (Vector2.Distance(transform.position, posB.position) < 0.1f)
+        {
+            movePos = posA;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, movePos.position, moveSpeed * Time.deltaTime);
+    }
+}
